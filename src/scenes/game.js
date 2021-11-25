a0b60e15f5d717f22aa499dc553dc78cbf8022b4
randomguy70 <onlychessfreeze0515@gmail.com>
2021-11-25 15:23:59 -0600

    initialising stuff

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,3 +1,6 @@
+var player;
+var cursors;
+
 class GameScene extends Phaser.Scene
 {
 	constructor ()
@@ -9,7 +12,15 @@ class GameScene extends Phaser.Scene
 	preload ()
 	{
 		console.log('preloaded');
-		
+		this.load.spritesheet('running_soldier', 
+		'./src/assets/running_soldier.png', { frameWidth: 43, frameHeight: 47 }
+		);
+	}
+	
+	create ()
+	{
+		player = this.physics.add.sprite(10, 10, 'running_soldier');
+		player.setCollideWorldBounds(true);
 	}
 }
 
