8263be9a70aa30e403b72032d73b321e4b599cc8
randomguy70 <onlychessfreeze0515@gmail.com>
2021-11-25 16:22:19 -0600

    running animation working

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -5,13 +5,14 @@ class GameScene extends Phaser.Scene
 {
 	constructor ()
 	{
-		super({key: 'gameScene'});
 		console.log('constructed gameScene');
+		super({key: 'gameScene'});
 	}
 	
 	preload ()
 	{
 		console.log('preloaded');
+		
 		this.load.spritesheet('running_soldier', 
 		'./src/assets/running_soldier.png', { frameWidth: 43, frameHeight: 47 }
 		);
@@ -19,8 +20,25 @@ class GameScene extends Phaser.Scene
 	
 	create ()
 	{
-		player = this.physics.add.sprite(10, 10, 'running_soldier');
+		console.log('created');
+		
+		player = this.physics.add.sprite(10, 10, 'running_soldier').setScale(1.5);
 		player.setCollideWorldBounds(true);
+		player.setBounce(0.2);
+		
+		this.anims.create({
+			key: 'move',
+			frames: this.anims.generateFrameNumbers('running_soldier', { start: 0, end: 6 }),
+			frameRate: 10,
+			repeat: -1
+	  });
+	  
+	  cursors = this.input.keyboard.createCursorKeys();
+	}
+	
+	update ()
+	{
+		player.anims.play('move', true);
 	}
 }
 
