a808e6eeea162d74012b746a54683e5b2b80abcd
randomguy70 <onlychessfreeze0515@gmail.com>
2022-03-13 15:15:40 -0500

    fixed tilemap path

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -13,42 +13,41 @@ class GameScene extends Phaser.Scene
 	
 	preload ()
 	{
-		console.log('preloaded');
+		console.log('preloading...');
 		
-		this.load.image("tiles", '../assets/tilesets/Terrain.png');
-		this.load.tilemapTiledJSON("map", "../assets/tilemaps/map1.json");
+		this.load.image("tiles", '../src/assets/tilesets/Terrain.png');
+		this.load.tilemapTiledJSON("map", "../src/assets/tilemaps/map1.json");
+		
 	}
 	
 	create ()
 	{
-		console.log('created');
+		console.log('creating...');
 		
 		const map = this.make.tilemap({ key: "map" });
 		
-		// Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
-		// Phaser's cache (i.e. the name you used in preload)
 		const tileset = map.addTilesetImage("tileset", "tiles");
-
+		
 		// Parameters: layer name (or index) from Tiled, tileset, x, y
-		const worldLayer = map.createLayer("World", tileset, 0, 0);
+		const worldLayer = map.createLayer(0, tileset, 0, 0);
 		
-		var cursors = this.input.keyboard.createCursorKeys();
+		// var cursors = this.input.keyboard.createCursorKeys();
 		
-		var controlConfig = {
-			camera: this.cameras.main,
-			left: cursors.left,
-			right: cursors.right,
-			up: cursors.up,
-			down: cursors.down,
-			speed: 0.5
-    	};
+		// var controlConfig = {
+			// camera: this.cameras.main,
+			// left: cursors.left,
+			// right: cursors.right,
+			// up: cursors.up,
+			// down: cursors.down,
+			// speed: 0.5
+    	// };
 		 
-		 controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+		//  controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
 	}
 
 	update (time, delta)
 	{
-		controls.update(delta);
+		// controls.update(delta);
 	}
 }
 
